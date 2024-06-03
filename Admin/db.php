e93a2e58ba7419196da9c2c94a6a3fb9a8bf5bba
Mariel <trishia.sandiego@tup.edu.ph>
2024-06-04 00:03:34 +0800

    Create page for
    - User Profile
    - Brgy ID
    - Brgy Cert
    - History of Transaction

--- Admin/db.php
+++ Admin/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bms1";
+$dbname = "barangay_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
---
+++ Admin/history_transaction.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="sidenavbar.css">
+    <link rel="stylesheet" href="history_transaction.css">
+    <title>History of Transaction</title> 
+</head>
+
+<body>
+<header>
+    <ul class="sidenav">
+        <!-- Profile Section -->
+        <li class="logo-profile">
+            <img src="images\global_image\logo-grabage-G-circle.png" alt="Profile Picture" class="logo-profile-photo">
+            <div class="text-profile"><p style="color: #fff;">Resident</p></div>
+        </li>
+        <div class="tools">
+            <!-- Left Nav Bar -->
+            <li class="sidebar"><a href="edit_profile.php" style="text-decoration: none;">Profile User </a></li>
+            <li class="sidebar"><a href="brgy_id_req.php" style="text-decoration: none;">ID Request</a></li>
+            <li class="sidebar"><a href="brgy_cert_req.php" style="text-decoration: none;">Certificate </a></li>
+            <li class="sidebar-active"><a href="history_transaction.php" style="text-decoration: none;">History of Transactions </a></li>
+            <!-- Logout Button -->
+            <li class="sidebar">
+                <a href="logout.php" style="text-decoration: none;">Logout</a>
+            </li>
+        </div>
+        <li class="logo-profile ">
+            <img src="images\global_image\logo-grabage-text.png" alt="Profile Picture" class="logo-text-photo">
+        </li>
+    </ul>
+</header>
+
+<section class="main">
+    <h1>History of Transaction</h1>
+
+    <table class="table caption-top">
+    <caption>List of Transactions</caption>
+    <thead class="table-light">
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Type of Request</th>
+        <th scope="col">Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Barangay Certificate</td>
+        <td>Pending</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Barangay ID</td>
+        <td>Ready to pick up</td>
+        </tr>
+        
+    </tbody>
+    </table>
+    
+</section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="formchecker.js"></script>
+<script type="module" src="Firebase.js"></script>
+
+
+</body>
+</html>
